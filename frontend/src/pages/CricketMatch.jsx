980e03cf446f5be8d08c9bf23d56c0bc9e13a130
Anadi Sharma <sharma.130@iitj.ac.in>
2024-11-30 20:21:50 +0530

    Adding condition for match tile widget displays

--- frontend/src/pages/CricketMatch.jsx
+++ frontend/src/pages/CricketMatch.jsx
@@ -3,10 +3,14 @@ import MatchCarousel from '../components/MatchCarousel'
 import Navbar from '../components/Navbar'
 
 export default function CricketMatch() {
+  const matches = [
+    {"match":"Men's T20I Tri-Series ","time":"01:00 PM","team1":"India","team2":"Australia","date":"2021-12-27","team1Image":"src/assets/india_team.png","team2Image":"src/assets/aus_team.png"},
+    {"match":"Men's T20I Tri-Series ","time":"01:00 PM","team1":"England","team2":"South Africa","date":"2021-12-27","team1Image":"src/assets/india_team.png","team2Image":"src/assets/aus_team.png"},
+  ]
   return (
     <>
     <Navbar selectedValue="cricket"/>
-    <MatchCarousel type="contest-join" />
+    <MatchCarousel type="contest-join" matches={matches} />
     </>
   )
 }
--- frontend/src/pages/CricketMatchContest.jsx
+++ frontend/src/pages/CricketMatchContest.jsx
@@ -3,10 +3,13 @@ import MatchCarousel from '../components/MatchCarousel'
 import Navbar from '../components/Navbar'
 
 export default function CricketMatchContest() {
+    const matches = [
+        {"match":"Men's T20I Tri-Series ","time":"01:00 PM","team1":"India","team2":"Australia","date":"2021-12-27","team1Image":"src/assets/india_team.png","team2Image":"src/assets/aus_team.png"},
+    ]
   return (
     <>
     <Navbar selectedValue="cricket"/>
-    <MatchCarousel type="contest-prize" />
+    <MatchCarousel type="contest-prize" matches={matches} />
     <div id='cricket-match-contests'>
       <br />
       <h4 style={{ fontSize: "23px" }}>Select a contest to join</h4>
