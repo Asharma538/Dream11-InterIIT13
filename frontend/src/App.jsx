569f75d08f2b57c62d75a5bac3550dbfa7c3167f
Naman <naman280104@gmail.com>
2024-11-30 19:09:43 +0530

    Cricket Match apge added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import MatchCarousel from './components/MatchCarousel';
 import Home from './pages/Home';
 import Cricket from './pages/Cricket';
 import {Routes, Route, BrowserRouter as Router} from 'react-router-dom';
+import CricketMatch from './pages/CricketMatch';
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cricket" element={<Cricket />} />
+        <Route path="/cricket-match" element={<CricketMatch />} />
       </Routes>
     </Router>
   )
