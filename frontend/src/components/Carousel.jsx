5c06387ffb78d44d803d8fa22119799a16ef7f21
Naman <naman280104@gmail.com>
2024-11-30 14:30:12 +0530

    carousel done and some home content

---
+++ frontend/src/components/Carousel.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+
+const ImageCarousel = () => {
+    const images = [
+    'src/assets/carousel-1.png',
+    'src/assets/carousel-1.png',
+    'src/assets/carousel-1.png',
+    ]
+  return (
+    <Carousel showThumbs={false} autoPlay infiniteLoop>
+        {images.map((image, index) => (
+            <div key={index}>
+            <img src={image} alt="carousel" />
+            </div>
+        ))}
+    </Carousel>
+  );
+};
+
+export default ImageCarousel;
