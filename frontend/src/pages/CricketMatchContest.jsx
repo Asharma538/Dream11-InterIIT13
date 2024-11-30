56a35964db888e493a4c7a83feb812b6cd894fe6
Anadi Sharma <sharma.130@iitj.ac.in>
2024-11-30 22:21:00 +0530

    Adding table for contest

--- frontend/src/pages/CricketMatchContest.jsx
+++ frontend/src/pages/CricketMatchContest.jsx
@@ -6,6 +6,10 @@ export default function CricketMatchContest() {
     const matches = [
         {"match":"Men's T20I Tri-Series ","time":"01:00 PM","team1":"India","team2":"Australia","date":"2021-12-27","team1Image":"src/assets/india_team.png","team2Image":"src/assets/aus_team.png"},
     ]
+    const [selectedTab, setSelectedTab] = React.useState("prizes");
+    const changeSelectedTab = (tab) => {
+        setSelectedTab(tab);
+    }
   return (
     <>
     <Navbar selectedValue="cricket"/>
@@ -34,7 +38,7 @@ export default function CricketMatchContest() {
           <div style={{ height: "4px" }}></div>
           <div style={{ color: "gray", display: "flex", justifyContent: "space-between", fontSize: "14px" }}>
             <div>32,12,123 left</div>
-            <div>32,12,123 spots</div>
+            <div>64,24,246 spots</div>
           </div>
           <br /><br />
           <div style={{display:"flex", justifyContent:"space-between"}}>
@@ -42,11 +46,10 @@ export default function CricketMatchContest() {
               <div>Prize: Rs. 1 crore</div>
               <div>Winners: 66%</div>
             </div>
-            <button style={{"padding":"10px","border":"1px solid gray","borderRadius":"30px",backgroundColor: "#1DC120"}}>
+            <button style={{"padding":"10px","border":"none","borderRadius":"30px",backgroundColor: "#1DC120",color: "white"}}>
               Join for Rs. 49 Only
             </button>
           </div>
-
         </div>
         <div className='cricket-match-contest-card'>
         <h3>Quiz</h3>
@@ -68,20 +71,36 @@ export default function CricketMatchContest() {
               <div>Prize: 10 Dream Coins</div>
               <div>Winners: 30%</div>
             </div>
-            <button style={{"padding":"10px","border":"1px solid gray","borderRadius":"30px",backgroundColor: "#1DC120"}}>
+            <button style={{"padding":"10px","border":"none","borderRadius":"30px",backgroundColor: "#1DC120",color: "white"}}>
               Attempt Quiz
             </button>
           </div>
-
         </div>
       </div>
     </div>
     <div>
       <div id='cricket-match-contest-details-tabs'>
-          <div id='cricket-match-contest-details-tab'>Prizes</div>
-          <div id='cricket-match-contest-details-tab'>Winners</div>
-          <div id='cricket-match-contest-details-tab'>Rules</div>
+          <div className='cricket-match-contest-details-tab' onClick={()=>changeSelectedTab("prizes")} style={{backgroundColor: selectedTab=="prizes"?"red":"inherit"}}>Prizes</div>
+          <div className='cricket-match-contest-details-tab' onClick={()=>changeSelectedTab("winners")} style={{backgroundColor: selectedTab=="winners"?"red":"inherit"}}>Winners</div>
+          <div className='cricket-match-contest-details-tab' onClick={()=>changeSelectedTab("rules")} style={{backgroundColor: selectedTab=="rules"?"red":"inherit"}}>Rules</div>
       </div>
+      <div id='cricket-match-contest-table'>
+        <table>
+            <tr>
+                <th>Rank</th>
+                <th>Winnings</th>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>Rs. 1 Crore</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>Rs. 50 Lakh</td>
+            </tr>
+        </table>
+      </div>
+      <div style={{"height":"40px"}}></div>
     </div>
 
 
