0765efb31b3807c2161298f8c5c2572195290fbd
Naman <naman280104@gmail.com>
2024-11-30 19:10:58 +0530

    package lock deleted

---
+++ frontend/src/pages/CricketMatch.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function CricketMatch() {
+  return (
+    <div id='cricket-match-contests'>
+        <br />
+        <h4 style={{fontSize:"23px"}}>Select a contest to join</h4>
+        <br />
+        <div id='cricket-match-contest-both-cards'>
+          <div className='cricket-match-contest-card'>
+             f
+          </div>
+          <div className='cricket-match-contest-card'>
+             f
+          </div>
+        </div>
+    </div>
+  )
+}
